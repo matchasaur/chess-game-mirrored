0cd8a489ad55c29aa5f488849866b877c7f6adaf
cwong165 <77028662+cwong165@users.noreply.github.com>
2021-03-07 03:17:42 -0800

    Update player_test.cpp

--- player_test.cpp
+++ player_test.cpp
@@ -3,24 +3,41 @@
 
 TEST(PLAYER, SIMPLETEST1)
 {
-  Player* a = new Player("JIMMY",true,true) ;
+  HumanPlayer* a = new HumanPlayer("JIMMY",true) ;
   EXPECT_EQ(true, a->isWhiteSide());
+  EXPECT_EQ(true, a->isHumanPlayer());
+  a->log_in("yoyoyoyoyo"); //First time password = set password // You must have a password before exporting data.
+  EXPECT_EQ(true, a->check_log_in());
+  EXPECT_EQ(false, a->cheatenable());
+  EXPECT_EQ("0", a->get_ID());
+  EXPECT_EQ("???", a->print_status());
+  EXPECT_EQ("JIMMY", a->get_name());
+  EXPECT_EQ("BEGINER", a->get_level());
+
+
 }
+
 
 TEST(PLAYER, SIMPLETEST2)
 {
-  Player* a = new Player("JIMMY",true,true) ;
-  EXPECT_EQ(true, a->isHumanPlayer());
+  AI* a = new AI("JIMMY",true) ;
+  EXPECT_EQ(true, a->isWhiteSide());
+  EXPECT_EQ(false, a->isHumanPlayer());
+  EXPECT_EQ(true, a->cheatenable());
+  EXPECT_EQ("0", a->get_ID());
+  EXPECT_EQ("JIMMY", a->get_name());
+  EXPECT_EQ("BEGINER", a->get_level());
+  EXPECT_EQ("???", a->print_status());
+  a->cheat_instant_max_level_true();
+  a->update_level();
+  EXPECT_EQ("The Global Elite", a->get_level());
+  EXPECT_EQ("???", a->print_status());
+  a->cheat_instant_min_level_true();
+  a->update_level();
+  EXPECT_EQ("Silver Elite Master", a->get_level());
+  EXPECT_EQ("???", a->print_status());
+
 }
-
-
-
-
-
-
-
-
-
 
 
 
