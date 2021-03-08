4d9c09a41df14859fdde9e3f5deace981cd7e6c4
cwong165 <77028662+cwong165@users.noreply.github.com>
2021-03-08 00:18:39 -0800

    Update player_test.cpp

--- player_test.cpp
+++ player_test.cpp
@@ -1,5 +1,6 @@
 #include "gtest/gtest.h"
-#include "../header/player.hpp"
+#include "player.hpp"
+
 
 TEST(PLAYER, SIMPLETEST1)
 {
@@ -9,11 +10,9 @@ TEST(PLAYER, SIMPLETEST1)
   a->log_in("yoyoyoyoyo"); //First time password = set password // You must have a password before exporting data.
   EXPECT_EQ(true, a->check_log_in());
   EXPECT_EQ(false, a->cheatenable());
-  EXPECT_EQ("???", a->print_status());
+  EXPECT_EQ("Player JIMMY is now BEGINER. JIMMY is a Human Player with a winrate of 0.000000 in 0 games", a->print_status());
   EXPECT_EQ("JIMMY", a->get_name());
   EXPECT_EQ("BEGINER", a->get_level());
-
-
 }
 
 
@@ -25,19 +24,39 @@ TEST(PLAYER, SIMPLETEST2)
   EXPECT_EQ(true, a->cheatenable());
   EXPECT_EQ("JIMMY", a->get_name());
   EXPECT_EQ("BEGINER", a->get_level());
-  EXPECT_EQ("???", a->print_status());
+  EXPECT_EQ("Player JIMMY is now BEGINER. JIMMY is a Computer Player with a winrate of 0.000000 in 0 games", a->print_status());
   a->cheat_instant_max_level_true();
   a->update_level();
   EXPECT_EQ("The Global Elite", a->get_level());
-  EXPECT_EQ("???", a->print_status());
+  EXPECT_EQ("Player JIMMY is now The Global Elite. JIMMY is a Computer Player with a winrate of 1.000000 in 100 games", a->print_status());
   a->cheat_instant_min_level_true();
   a->update_level();
   EXPECT_EQ("Silver Elite Master", a->get_level());
-  EXPECT_EQ("???", a->print_status());
+  EXPECT_EQ("Player JIMMY is now Silver Elite Master. JIMMY is a Computer Player with a winrate of 0.000000 in 100 games", a->print_status());
+ 
 
 }
 
+TEST(PLAYER, SIMPLETEST2)
+{
+  ComputerPlayer* a = new ComputerPlayer("JIMMY",true) ;
+  EXPECT_EQ(true, a->isWhiteSide());
+  EXPECT_EQ(false, a->isHumanPlayer());
+  EXPECT_EQ(true, a->cheatenable());
+  EXPECT_EQ("JIMMY", a->get_name());
+  EXPECT_EQ("BEGINER", a->get_level());
+  EXPECT_EQ("Player JIMMY is now BEGINER. JIMMY is a Computer Player with a winrate of 0.000000 in 0 games", a->print_status());
+  a->cheat_instant_max_level_true();
+  a->update_level();
+  EXPECT_EQ("The Global Elite", a->get_level());
+  EXPECT_EQ("Player JIMMY is now The Global Elite. JIMMY is a Computer Player with a winrate of 1.000000 in 100 games", a->print_status());
+  a->cheat_instant_min_level_true();
+  a->update_level();
+  EXPECT_EQ("Silver Elite Master", a->get_level());
+  EXPECT_EQ("Player JIMMY is now Silver Elite Master. JIMMY is a Computer Player with a winrate of 0.000000 in 100 games", a->print_status());
+ 
 
+}
 
 
 int main(int argc, char **argv) {
