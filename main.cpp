b1030d5864a91beebbf3993983e0500cc55a75d3
Jared.Tan <Jtanu45@gmail.com>
2021-03-10 15:37:38 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,46 +4,8 @@
 using namespace std;
 
 int main(){
-    Game* f = new Game();
-    f->PrintMenu(f);
-    //f.game_start();
-
+    Game chessGame;
+    chessGame.game_start();
+	
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //Board* f = new Board();
-    //f->printBoard();
-
-    //int startX,startY,endX,endY;
-    //f->kingCaptured = false;
-    //while(f->kingCaptured != true){
-    //cin >> startX;
-    //cin >> startY;
-    //cin >> endX;
-    //cin >> endY;
-
-    //f->move(f, f->getBox(startX, startY), f->getBox(endX,endY));
-    //f->printBoard();
-
-    //}
-	//delete f;
