14416aadb04d0ec347e8790d14ee3f7e37c9b205
danialmirza99 <danialmirza99@gmail.com>
2021-03-10 15:12:07 -0800

    Added two more tests

--- factory_test.hpp
+++ factory_test.hpp
@@ -16,4 +16,20 @@ TEST(FactoryTest, simplFactTest){
 }
 
 
+TEST(FactoryTest, simplFactTest2){
+        Board* f = new Board();
+        MoveFactory t;
+        canMove* mover = t.make_canMove(f->getBox(3,0)->getPiece());
+        mover->move(f, f->getBox(3,0), f->getBox(6,0));
+        delete f;
+}
+
+TEST(FactoryTest, simplFactTest3){
+        Board* f = new Board();
+        MoveFactory t;
+        canMove* mover = t.make_canMove(f->getBox(5,5)->getPiece());
+        mover->move(f, f->getBox(5,0), f->getBox(3,5));
+        delete f;
+}
+
 #endif
