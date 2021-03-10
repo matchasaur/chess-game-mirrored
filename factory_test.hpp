0314cfc609ebbff5cc8ee6eea075bca0cf84ca70
danialmirza99 <danialmirza99@gmail.com>
2021-03-10 14:49:03 -0800

    changed factory test but still have errors

--- factory_test.hpp
+++ factory_test.hpp
@@ -5,11 +5,26 @@
 #include <iostream>
 using namespace std;
 
+
+
 TEST(FactoryTest, PawnTest){
-	char* test_val[1]; test_val[0] = "./test";
-        MoveFactory* factoryTest = new MoveFactory(test_val, 1);
-	string results = test->history.pop();
+	char* test_val[2]; test_val[0] = "./test"; test_val[1] = "P";
+        MoveFactory* f = new MoveFactory();
+	canMove* test = f->make_canMove(test_val, 2);
+	string results = test->make_canMove();
         EXPECT_EQ("P", results);
 }
+
+
+
+TEST(FactoryTest, PawnTest){
+        char* test_val[2]; test_val[0] = "./test"; test_val[1] = "P";
+	Board* f = new Board();
+	MoveFactory* test = f->move();
+        string results = test->make_canMove();
+        EXPECT_EQ("P", results);
+}
+
+
 
 #endif
