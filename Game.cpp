b511813bcaccdd133e09c11478ad01603a51cc45
cwong165 <77028662+cwong165@users.noreply.github.com>
2021-03-10 17:51:05 -0800

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -132,6 +132,7 @@ void Game::game_start() {
       //cout << "DEBUG3";
       string temp;
       cout << "There are more players waiting to play with you in the queue, do you want to continute? Do you want to see your rank? (Y/N/R)\n";
+      cin >> temp;
       cin.ignore();
       if (temp == "Y") {
         game_start();
